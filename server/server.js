803f77605563b68ee85bde2a5f84fe7d8a24d2b2
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2024-06-25 23:12:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { Server } = require("socket.io");
 const { v4: uuidV4 } = require('uuid');
 const http = require('http');
+const cors = require('cors');
+
 
 const app = express(); 
 app.use(cors({
