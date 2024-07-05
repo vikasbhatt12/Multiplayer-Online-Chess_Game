099f126477efdf99701109bfe05ca6685dae2a71
vikasbhatt12 <vikasbhatt12082003@gmail.com>
2024-07-05 15:36:30 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io('https://multiplayer-online-chess-game-server.vercel.app'); // initialize websocket connection
+const socket = io('https://elitechess.onrender.com'); // initialize websocket connection
 
 export default socket;
